6fe468438f7290b51bf7ce6bb3f66e26c2a59d50
Matteo <daylof05@gmail.com>
2022-11-09 16:28:25 +0100

    page partie v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <TextField
+          id="party-name"
+          margin="normal"
+          label=""
+          defaultValue="Nom de la partie"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <br></br>
+        <TextField
+          id="party-code"
+          margin="normal"
+          label=""
+          defaultValue="Code:"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <br></br><br></br>
+        <Button variant="contained">Gérer les sprints</Button>
+        <br></br><br></br>
+        <Button variant="contained">Gérer les stories</Button>
+        <br></br><br></br>
+        <Button variant="contained">Participer au Daily</Button>
+        <br></br><br></br>
+        <Button variant="contained">Gérer les participants</Button>
+        <br></br><br></br>
+        <Button variant="contained">Gérer le Daily</Button>
+        <br></br><br></br>
+        <Button variant="contained">Quitter la partie</Button>
+      </center>
     </div>
   );
 }
