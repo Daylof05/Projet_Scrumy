ec229eaec230475a34370db66bae4c1915a60af6
Matteo <daylof05@gmail.com>
2022-11-10 16:41:20 +0100

    fix: index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import CreateParty from './CreateParty'
+// import JoinParty from './JoinParty'
+// import Sprints from './Sprints'
+// import Stories from './Stories'
+// import DailyQuestionnaire from './DailyQuestionnaire'
+// import Questionnaire from './Questionnaire'
+// import Accueil from './Accueil'
+// import Daily from './Daily'
+// import NewDaily from './NewDaily';
+// import NewDaily from './NewDaily';
+// import Participants from './Participants';
+import ResultatQuestionnaire from './ResultatQuestionnaire'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <CreateParty/> */}
+    {/* <JoinParty/> */}
+    {/* <Sprints/> */}
+    {/* <Stories/> */}
+    {/* <DailyQuestionnaire/> */}
+    {/* <Questionnaire/> */}
+    {/* <Accueil/> */}
+    {/* <Daily/> */}
+    {/* <NewDaily/> */}
+    {/* <Participants/> */}
+    <ResultatQuestionnaire/>
   </React.StrictMode>
 );
 
